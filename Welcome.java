6f5ced2ad1455bac2eaf77eb4f0e4703575ed369
Dinesh Kumar Sahu <you@example.com>
2025-08-03 00:45:40 +0530

    Updated Welcome.java

---
+++ Welcome.java
@@ -0,0 +1,7 @@
+public class Welcome {
+    public static void main(String[] args) {
+        System.out.println("Welcom to Java Programmign");
+        System.out.println("This is my favarite language");
+    }
+    
+}
